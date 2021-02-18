f5ff6d8d485b5a497b44405f1bd3a6dca8d5019e
Valentina Guerrero <valentinaguerrero@Valentinas-MacBook-Pro.local>
2021-02-17 21:27:10 -0500

    Main changes

--- main.js
+++ main.js
@@ -1,6 +1,59 @@
 
 let content = document.getElementById('content-div-5');
 let bar = document.getElementById('getID');
+let nav = document.getElementById('nav');
+
+
+
+// get the element to ani
+var navHeight = nav.clientHeight;
+
+// listen for scroll event and call animate function
+document.addEventListener('scroll', animate);
+
+// check if element is in view
+function inView() {
+  // get window height
+  var windowHeight = window.innerHeight;
+  // get number of pixels that the document is scrolled
+  var scrollY = window.scrollY || window.pageYOffset;
+  
+  // get current scroll position (distance from the top of the page to the bottom of the current viewport)
+  var scrollPosition = scrollY + windowHeight;
+  console.log('scroll')
+
+  console.log(scrollPosition);
+
+  // get element position (distance from the top of the page to the bottom of the element)
+  var elementPosition = nav.getBoundingClientRect().top + scrollY + navHeight;
+
+console.log('elementPosition')
+  console.log(elementPosition);
+  
+  if (elementPosition == 92){
+    return false;
+  }
+  if (scrollPosition > elementPosition) {
+    return true;
+  }
+  
+  return false;
+}
+
+// animate element when it is in view
+function animate() {
+  // is element in view?
+  if (inView()) {
+      // element is in view, add class to element
+      nav.classList.add('box-shadow');
+  }
+
+  else if(inView() == false){
+    nav.classList.remove('box-shadow');
+    console.log('tets nav')
+  }
+}
+
 
 
 
@@ -8,9 +61,6 @@ window.addEventListener("scroll", scroll);
 
 function scroll (){
   
-  	console.log(content.scrollHeight)
-  	console.log(content.scrollTop)
-  	console.log(content.clientHeight)
   
 
 }
@@ -22,7 +72,7 @@ window.addEventListener('wheel', checkScrollDirection);
 
 function checkScrollDirection(event) {
   if (checkScrollDirectionIsUp(event)) {
-  	console.log('UP');
+  
   	content.scrollBy(0, -17);
   	
   	return true
